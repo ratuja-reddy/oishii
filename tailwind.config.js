1f602fc16879e4085bfe9b3c222762ae595dd0f8
Ratuja Reddy <ratuja@runna.com>
2025-10-26 22:49:32 +0000

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.{html,js}", "./**/templates/**/*.html"],
-  theme: { extend: {} },
+  theme: { 
+    extend: {
+      colors: {
+        'cream': '#FAF2EF',
+        'terracotta': '#CA6145',
+        'dark-teal': '#2F4F4F',
+        'dark-text': '#1E1E1E',
+        'white': '#FFFFFF',
+      }
+    } 
+  },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
 
